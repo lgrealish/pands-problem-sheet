68c1864d94a6a6e04e6bd526c92434de7d0b5ff6
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-04-04 17:11:04 +0100

    add more comments to python-squareroot.py

--- python-squareroot.py
+++ python-squareroot.py
@@ -1,27 +1,32 @@
-# weekday.py
+# python-squareroot.py
 # Author: Linda Grealish
 # This program takes a positive floating-point number as input 
-# and outputs an approximation of its square root
+# and outputs an approximation of its square root using Newtons Method
 # week 6 task
+# https://stackoverflow.com/questions/28733759/python-square-function-using-newtons-algorithm
 
+# set input to float type
 n = float (input("Enter a positive number: "))
-#format_n = "{:.2f}".format(n)
-approx_sqrt = 0.5*n
-print(f"Approximate square root of {n} is : {approx_sqrt}")
+
+# if n < 0:
+    #print ("Try again! \nEnter a positive number: ")
 
 
 def newtonSqrt(n):
+    # start by assuming approx square root is half the number
     approx=0.5*n
+    # find a better value by using the following calculation
     better=0.5*(approx+n/approx)
+    # while the better value found is not equal to the approxassumed approx then 
+    # recalculate the better value using calculation better=0.5*(approx+n/approx)
     while better!=approx:
         approx=better
         better=0.5*(approx+n/approx)
     return approx
 
-
 result = {newtonSqrt(n)}
 
-
 print (f"The square root of {n} is : ", round (newtonSqrt(n),3) )
+
 
 
