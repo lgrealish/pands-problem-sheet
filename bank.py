3775ce3418dfdb54b58dd76ea9e9582f0318ad81
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-04-06 13:19:13 +0100

    Update comments in bank.py

--- bank.py
+++ bank.py
@@ -4,10 +4,14 @@
 # answer with a € symbol and decimal point
 # week 2 task
 
+# user prompted to input an amount in cent
 amount1 = int(input('Please enter amount1 (in cent):'))
-
+# user prompted to input a second amount in cent
 amount2 = int(input('Please enter amount2 (in cent):'))
 
+# the sum function adds the 2 amounts input, divides the total by 100 to 
+# convert to € and cent and returns the amount as a floating number
 sum = float((amount1 + amount2) / 100)
 
+# displays the result of the above calculation to 2 decimal places
 print (f"The sum of these is €" + format(format(sum, ',.2f')))
