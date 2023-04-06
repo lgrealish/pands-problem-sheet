559365d64ac0341d3500dfd4c7947059e12da049
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-04-06 13:04:08 +0100

    Fix issues and add comments to plottask.py

--- plottask.py
+++ plottask.py
@@ -6,16 +6,29 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
+# first we define the parameters for the histogram, mean, standard deviation and values
 mean_x = 5
 std_x = 2
 values_x = 1000
 
-x = np.random.normal(mean_x, std_x, size=values_x)
-sns.histplot(x, color='r')
+# normData using the random generator from numpy to create a histogram of a normal distribution
+normData = np.random.normal(mean_x, std_x, size=values_x)
+# use plt.hist from matploylib.pyplot to plot the parameters 
+plt.hist(normData, color = "b",label= "Normal Distribution")
 
-h = x^3
-plt.plot (x, h, "r.", label="h(x) = x^3")
+# define the variable 'xpoints' in the range [0,10]
+xpoints = np.array(range(0,10))
+# define ypoints as xpoints^3
+ypoints = xpoints**3
+# use plt.plotfunction to display line plot of 'ypoints'
+plt.plot(ypoints, color ="r", label = ("x^3"))
 
+# assign plot title, labels to x and y axis and add legend
+plt.title ("Week 8 task")
+plt.xlabel ("x value")
+plt.ylabel ("Function")
+plt.legend()
+
+# using plt.show() to return visual of the above input data
 plt.show()
