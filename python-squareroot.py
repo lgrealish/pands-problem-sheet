c0c8f83e02e7519ed27b41cfaf50bbf56cbaaf8d
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-04-08 15:23:33 +0100

    Fix validation code in python-squareroot.py

--- python-squareroot.py
+++ python-squareroot.py
@@ -9,26 +9,26 @@
 # set input to float type
 n = float (input("Enter a positive number: "))
 
-# if n < 0:
-    #print ("Try again! \nEnter a positive number: ")
+while n < 0:
+    n = float (input("Try again! \nEnter a positive number: "))
 
-
-def newtonSqrt(n):
-    # start by assuming approx square root is half the number
-    approx=0.5*n
-    # find a better value by using the following calculation
-    better=0.5*(approx+n/approx)
-    # while the better value found is not equal to the approxassumed approx then 
-    # recalculate the better value using calculation better=0.5*(approx+n/approx)
-    while better!=approx:
-        approx=better
+else:
+    def newtonSqrt(n):
+        # start by assuming approx square root is half the number
+        approx=0.5*n
+        # find a better value by using the following calculation
         better=0.5*(approx+n/approx)
-    return approx
+        # while the better value found is not equal to the approxassumed approx then 
+        # recalculate the better value using calculation better=0.5*(approx+n/approx)
+        while better!=approx:
+            approx=better
+            better=0.5*(approx+n/approx)
+        return approx
 
-result = {newtonSqrt(n)}
+    result = {newtonSqrt(n)}
 
-# print the reult and round the result to 3 decimal places using the round() function
-print (f"The square root of {n} is : ", round (newtonSqrt(n),3) )
+    # print the reult and round the result to 3 decimal places using the round() function
+    print (f"The square root of {n} is : ", round (newtonSqrt(n),3) )
 
 
 
